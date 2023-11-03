c263d276bf5c0320aedde3dbd35adeadcb0e9252
JUAN SEBASTIAN CEPEDA SARAY <juan.cepeda-s@mail.escuelaing.edu.co>
2023-11-03 02:16:26 +0000

    Deleted magicBrushstrokes

--- src/main/java/edu/eci/arsw/magicBrushstrokes/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.arsw.magicBrushstrokes.services;
-
-import edu.eci.arsw.magicBrushstrokes.model.Player;
-
-public interface UserService {
-
-    public void savePlayer (Player player);
-    
-}
