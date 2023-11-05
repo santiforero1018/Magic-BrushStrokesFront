072b9e589d4d76e3cec89436a6f162f8dfb462e9
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-11-05 13:27:07 -0500

    Sistema distribuido(FrontEnd), revisar Real Tiem

---
+++ server.js
@@ -0,0 +1,26 @@
+const SockJS = require('sockjs-client');
+const Stomp = require('stomp-websocket');
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+const PORT = 3000; 
+
+app.use(cors({
+  origin: 'http://192.168.1.11:9090',
+}));
+
+app.use(express.static(path.join(__dirname, 'resources/static')));
+
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules'), {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.js')) {
+      res.set('Content-Type', 'application/javascript');
+    }
+  },
+}));
+
+app.listen(PORT, () => {
+  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+});
